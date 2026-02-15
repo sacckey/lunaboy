0c8cb89c7950ccdf4c1439c9082f8f3116952463
sacckey <sackeyad24@gmail.com>
2026-02-15 23:33:13 +0900

    feat: add audio functionality and integrate with emulator

--- native/lib/lunaboy_native.c
+++ native/lib/lunaboy_native.c
@@ -36,3 +36,89 @@ void lunaboy_free_sdl_event(SDL_Event *event) {
 bool lunaboy_texture_is_null(SDL_Texture *texture) {
   return texture == NULL;
 }
+
+bool lunaboy_audio_stream_is_null(SDL_AudioStream *stream) {
+  return stream == NULL;
+}
+
+static SDL_AudioDeviceID g_audio_device = 0;
+static SDL_AudioStream *g_audio_stream = NULL;
+
+bool lunaboy_audio_open(int sample_rate, int channels) {
+  if (g_audio_stream != NULL) {
+    SDL_DestroyAudioStream(g_audio_stream);
+    g_audio_stream = NULL;
+  }
+  if (g_audio_device != 0) {
+    SDL_CloseAudioDevice(g_audio_device);
+    g_audio_device = 0;
+  }
+
+  SDL_AudioSpec src_spec;
+  SDL_zero(src_spec);
+  src_spec.format = SDL_AUDIO_F32;
+  src_spec.channels = channels;
+  src_spec.freq = sample_rate;
+
+  g_audio_device =
+      SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &src_spec);
+  if (g_audio_device == 0) {
+    return false;
+  }
+
+  SDL_AudioSpec dst_spec = src_spec;
+  int sample_frames = 0;
+  if (!SDL_GetAudioDeviceFormat(g_audio_device, &dst_spec, &sample_frames)) {
+    dst_spec = src_spec;
+  }
+
+  g_audio_stream = SDL_CreateAudioStream(&src_spec, &dst_spec);
+  if (g_audio_stream == NULL) {
+    SDL_CloseAudioDevice(g_audio_device);
+    g_audio_device = 0;
+    return false;
+  }
+
+  if (!SDL_BindAudioStream(g_audio_device, g_audio_stream)) {
+    SDL_DestroyAudioStream(g_audio_stream);
+    g_audio_stream = NULL;
+    SDL_CloseAudioDevice(g_audio_device);
+    g_audio_device = 0;
+    return false;
+  }
+
+  if (!SDL_ResumeAudioDevice(g_audio_device)) {
+    SDL_DestroyAudioStream(g_audio_stream);
+    g_audio_stream = NULL;
+    SDL_CloseAudioDevice(g_audio_device);
+    g_audio_device = 0;
+    return false;
+  }
+
+  return true;
+}
+
+bool lunaboy_audio_queue(const void *ptr, int len_bytes) {
+  if (g_audio_stream == NULL) {
+    return false;
+  }
+  return SDL_PutAudioStreamData(g_audio_stream, ptr, len_bytes);
+}
+
+int lunaboy_audio_queued_bytes(void) {
+  if (g_audio_stream == NULL) {
+    return 0;
+  }
+  return SDL_GetAudioStreamQueued(g_audio_stream);
+}
+
+void lunaboy_audio_close(void) {
+  if (g_audio_stream != NULL) {
+    SDL_DestroyAudioStream(g_audio_stream);
+    g_audio_stream = NULL;
+  }
+  if (g_audio_device != 0) {
+    SDL_CloseAudioDevice(g_audio_device);
+    g_audio_device = 0;
+  }
+}
